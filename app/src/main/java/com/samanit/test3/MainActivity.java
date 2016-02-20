faba41abdec972018e20a81c9a927a0cb531e9aa
rstamenkovic <radisavs@yahoo.com>
2016-02-20 15:38:57 +0100

    //Master2

--- app/src/main/java/com/samanit/test3/MainActivity.java
+++ app/src/main/java/com/samanit/test3/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Master1
+        //master2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
